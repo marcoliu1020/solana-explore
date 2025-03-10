7b901dc39d3c0718028aadd5b624b64e9f85c0d1
marco <a0931900802@gmail.com>
2025-03-11 05:04:38 +0800

    add totalBlocks

--- hooks/useBlocks.ts
+++ hooks/useBlocks.ts
@@ -1,13 +1,22 @@
-import useSWR from "swr"
 import { getBlocks, type QueryParams } from "@/apis/getBlocks"
+import { useState } from "react"
+import useSWR from "swr"
 
 export function useBlocks(queryParams: QueryParams = {}) {
   const args = ['/getBlocks', ...Object.values(queryParams)]
   const { data, error, isLoading, isValidating } = useSWR(args, () => getBlocks(queryParams), {
     keepPreviousData: true,
   })
+
+  // pagination
   const previousBlockNumber = data?.pagination.previous
   const nextBlockNumber = data?.pagination.next
+  
+  // set total blocks
+  const [totalBlocks, setTotalBlocks] = useState(0)
+  if (data && data.data.length > 0) {
+    if (!totalBlocks) setTotalBlocks(data.data[0].blockNumber)
+  }
 
-  return { data, error, isLoading, isValidating, previousBlockNumber, nextBlockNumber }
+  return { data, error, isLoading, isValidating, previousBlockNumber, nextBlockNumber, totalBlocks }
 }
