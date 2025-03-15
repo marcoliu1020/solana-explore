7b09c8ec9e01855ba0432e242e70d50dc02c6532
marco <a0931900802@gmail.com>
2025-03-15 19:07:44 +0800

    add comment

--- hooks/useBlocks.ts
+++ hooks/useBlocks.ts
@@ -1,6 +1,11 @@
 import { getBlocks, type QueryParams } from '@/apis/getBlocks'
 import useSWR from 'swr'
 
+// 如果你要搜尋「區塊 1」，參數設定如下：
+// 1. from: 1
+// 2. size: 5
+// 3. reverse: true **沒有開啟的話，會找不到**
+
 export function useBlocks(queryParams: QueryParams = {}) {
   const args = ['/getBlocks', ...Object.values(queryParams)]
   const { data, error, isLoading, isValidating } = useSWR(
