5c68c24e679c91f7b1e63b649f276a43532edf50
marco <a0931900802@gmail.com>
2025-03-15 21:40:08 +0800

    add type of number

--- apis/getSpecificBlock.ts
+++ apis/getSpecificBlock.ts
@@ -5,7 +5,9 @@ export type SpecificBlockResponse = Response<Block>
 
 export const url = new URL('blocks', SOLANA_FM_URL)
 
-export async function getSpecificBlock(blockNumber: string): Promise<Block> {
+export async function getSpecificBlock(
+  blockNumber: string | number,
+): Promise<Block> {
   const requestUrl = `${url}/${blockNumber}`
   const res = await fetch(requestUrl)
   const data: SpecificBlockResponse = await res.json()
