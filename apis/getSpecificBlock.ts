bb258fdac98d700938dddb0ea7f6804eb3b6e057
marco <a0931900802@gmail.com>
2025-03-15 19:36:20 +0800

    feat: getSpecificBlock()

---
+++ apis/getSpecificBlock.ts
@@ -0,0 +1,13 @@
+import type { Block, Response } from './types'
+import { SOLANA_FM_URL } from './urls'
+
+export type SpecificBlockResponse = Response<Block>
+
+export const url = new URL('blocks', SOLANA_FM_URL)
+
+export async function getSpecificBlock(blockNumber: string): Promise<Block> {
+  const requestUrl = `${url}/${blockNumber}`
+  const res = await fetch(requestUrl)
+  const data: SpecificBlockResponse = await res.json()
+  return data.result
+}
