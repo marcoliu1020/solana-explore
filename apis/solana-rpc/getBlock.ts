06cfb55038154907e9237dc5d34381f6240aa5e5
marco <a0931900802@gmail.com>
2025-03-17 20:42:27 +0800

    feat: use clusterApiUrl

--- apis/solana-rpc/getBlock.ts
+++ apis/solana-rpc/getBlock.ts
@@ -1,6 +1,6 @@
-import { Connection } from '@solana/web3.js'
+import { clusterApiUrl, Connection } from '@solana/web3.js'
 
-const solana = new Connection('https://api.mainnet-beta.solana.com')
+const solana = new Connection(clusterApiUrl('mainnet-beta'), 'confirmed')
 
 export const getBlock = async (blockNumber: number) => {
   const block = await solana.getBlock(blockNumber, {
