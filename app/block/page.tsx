45393716f1c25d0ad1df8b64adb56d06dc99fd3a
marco <a0931900802@gmail.com>
2025-03-21 00:25:29 +0800

    feat: fetch block detail data

--- app/block/page.tsx
+++ app/block/page.tsx
@@ -1,6 +1,7 @@
 import { getBlock } from '@/apis/solana-rpc/getBlock'
 import { BlockDetail } from '@/components/block-detail'
 import PageTitle from '@/components/page-title'
+import { TransactionsTable } from '@/components/transactions-list'
 import { toSolanaAmount } from '@/lib/utils'
 import { notFound } from 'next/navigation'
 
@@ -12,27 +13,45 @@ export default async function BlockPage(props: { searchParams: SearchParams }) {
   const searchParams = await props.searchParams
   const blockNumber = Number(searchParams?.blockNumber)
 
+  // check if block number is valid
   if (!blockNumber || isNaN(blockNumber) || blockNumber < 1) {
     notFound()
   }
 
+  // get block details
   const block = await getBlock(blockNumber)
+  if (!block) notFound()
   console.log(block)
 
-  if (!block) notFound()
+  // transform transactions
+  const transactions =
+    block.transactions.slice(0, 10).map((tx) => ({
+      signature: tx.transaction.signatures[0],
+      signer: tx.transaction.message.staticAccountKeys[0].toString(),
+      fee: tx.meta?.fee ?? 0,
+    })) ?? []
+
   return (
     <>
-      <main className="p-4">
-        <PageTitle title="Overview" />
-        <BlockDetail
-          blockNumber={blockNumber}
-          timestamp={block?.blockTime ?? 0}
-          blockHash={block?.blockhash ?? ''}
-          leader={block?.rewards?.[0]?.pubkey ?? ''}
-          rewardInSol={toSolanaAmount(block?.rewards?.[0]?.lamports ?? 0)}
-          transactions={block?.transactions?.length ?? 0}
-          previousBlock={block?.previousBlockhash ?? ''}
-        />
+      <main className="mx-auto max-w-screen-lg p-4">
+        <>
+          <PageTitle title="Block Details" />
+          <BlockDetail
+            className="mt-2"
+            blockNumber={blockNumber}
+            timestamp={block?.blockTime ?? 0}
+            blockHash={block?.blockhash ?? ''}
+            leader={block?.rewards?.[0]?.pubkey ?? ''}
+            rewardInSol={toSolanaAmount(block?.rewards?.[0]?.lamports ?? 0)}
+            transactions={block?.transactions?.length ?? 0}
+            previousBlock={block?.previousBlockhash ?? ''}
+          />
+        </>
+
+        <>
+          <PageTitle className="mt-4" title="Transactions" />
+          <TransactionsTable className="mt-2" transactions={transactions} />
+        </>
       </main>
     </>
   )
