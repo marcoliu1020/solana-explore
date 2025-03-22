96d32026b6e91fe8bcdb3edc70ee26fc593501cf
marco <a0931900802@gmail.com>
2025-03-23 00:18:08 +0800

    feat: add helper txDetailDTO

---
+++ app/transaction/helper.tsx
@@ -0,0 +1,78 @@
+import CopyClipboard from '@/components/copy-clipboard'
+import LinkWithColor from '@/components/link-with-color'
+import { toSolanaAmount } from '@/lib/utils'
+import type { VersionedTransactionResponse } from '@solana/web3.js'
+
+export const txDetailDTO = (transaction: VersionedTransactionResponse) => {
+  return [
+    {
+      title: 'Signature',
+      value: (
+        <>
+          <div className="flex items-center gap-2 md:justify-end">
+            <p className="overflow-hidden text-ellipsis">
+              {transaction.transaction.signatures[0]}
+            </p>
+            <CopyClipboard text={transaction.transaction.signatures[0]} />
+          </div>
+        </>
+      ),
+    },
+    {
+      title: 'Block',
+      value: (
+        <LinkWithColor href={`/block?blockNumber=${transaction.slot}`}>
+          {transaction.slot}
+        </LinkWithColor>
+      ),
+    },
+    {
+      title: 'Timestamp',
+      value: transaction.blockTime
+        ? new Date(transaction.blockTime * 1000).toUTCString()
+        : 'N/A',
+    },
+    {
+      title: 'Result',
+      value: transaction.meta?.err?.toString(),
+    },
+    {
+      title: 'Signer',
+      value: (
+        <div className="flex items-center gap-2 md:justify-end">
+          <p className="overflow-hidden text-ellipsis">
+            {transaction.transaction.message.staticAccountKeys[0].toString()}
+          </p>
+          <CopyClipboard
+            text={transaction.transaction.message.staticAccountKeys[0].toString()}
+          />
+        </div>
+      ),
+    },
+    {
+      title: 'Fee',
+      value: toSolanaAmount(transaction.meta?.fee ?? 0),
+    },
+    {
+      title: 'Compute Units Consumed',
+      value: transaction.meta?.computeUnitsConsumed,
+    },
+    {
+      title: 'Transaction Version',
+      value: transaction.version,
+    },
+    {
+      title: 'Previous Blockhash',
+      value: (
+        <div className="flex items-center gap-2 md:justify-end">
+          <p className="overflow-hidden text-ellipsis">
+            {transaction.transaction.message.recentBlockhash}
+          </p>
+          <CopyClipboard
+            text={transaction.transaction.message.recentBlockhash}
+          />
+        </div>
+      ),
+    },
+  ]
+}
