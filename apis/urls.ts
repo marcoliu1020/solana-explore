afdc8a6d77e91bfd8cc344efef295fd092730805
marco <a0931900802@gmail.com>
2025-03-23 22:42:44 +0800

    feat: update url

--- apis/urls.ts
+++ apis/urls.ts
@@ -1,2 +1,4 @@
 export const SOLANA_FM_URL = 'https://api.solana.fm/v0/'
-export const SOLANA_RPC_URL = 'https://api.mainnet-beta.solana.com'
+// export const SOLANA_RPC_URL = 'https://api.mainnet-beta.solana.com'
+export const SOLANA_RPC_URL =
+  'https://cosmopolitan-fittest-isle.solana-devnet.quiknode.pro/7c554eba1780c5f248e3a9dece060f0baa8ab5bb/'
