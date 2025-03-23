ec9f9aaec9cf2647e37ed28d6fe4e1fe5a93f55c
marco <a0931900802@gmail.com>
2025-03-23 22:44:20 +0800

    import search

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Search from '@/components/search'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Search className="mt-10" />
         {children}
       </body>
     </html>
