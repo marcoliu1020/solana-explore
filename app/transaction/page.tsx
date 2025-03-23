4d963d21f68118e5a8c52e1cbe8e70be978cf441
marco <a0931900802@gmail.com>
2025-03-23 16:49:01 +0800

    feat: show log messages

--- app/transaction/page.tsx
+++ app/transaction/page.tsx
@@ -1,7 +1,9 @@
 import { getTransaction } from '@/apis/solana-rpc/getTransaction'
-import TitleValueCard from '@/components/title-value-card'
-import { txDetailDTO } from './helper'
+import { MessagesList } from '@/components/messages-list'
 import PageTitle from '@/components/page-title'
+import TitleValueCard from '@/components/title-value-card'
+import { notFound } from 'next/navigation'
+import { txDetailDTO } from './helper'
 
 type SearchParams = Promise<{
   signature?: string
@@ -12,26 +14,34 @@ export default async function TransactionPage(props: {
 }) {
   const searchParams = await props.searchParams
   const transactionSignature = searchParams.signature
-
   if (!transactionSignature) {
-    return <div>Transaction not found</div>
+    return notFound()
   }
 
   const transaction = await getTransaction(transactionSignature)
-
   if (!transaction) {
-    return <div>Transaction not found</div>
+    return notFound()
   }
-
-  console.log(transaction)
-
   // tx detail DTO
   const txDetail = txDetailDTO(transaction)
+  // logs
+  const logMessages = transaction.meta?.logMessages
 
   return (
     <main className="mx-auto max-w-screen-lg p-4">
-      <PageTitle title="Transaction Details" />
-      <TitleValueCard className="mt-2" titleValues={txDetail} />
+      {/* tx detail */}
+      <div>
+        <PageTitle title="Transaction Details" />
+        <TitleValueCard className="mt-2" titleValues={txDetail} />
+      </div>
+
+      {/* logs */}
+      {logMessages && logMessages.length > 0 && (
+        <div className="mt-4">
+          <PageTitle title="Logs" />
+          <MessagesList className="mt-2" logs={logMessages} />
+        </div>
+      )}
     </main>
   )
 }
