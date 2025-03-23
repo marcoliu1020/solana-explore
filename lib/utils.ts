3a5846a471ca4e5ec39b602e76e1ffdae8e9bd5c
marco <a0931900802@gmail.com>
2025-03-23 19:49:09 +0800

    add helper toBase58 toHex

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { LAMPORTS_PER_SOL } from '@solana/web3.js'
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import bs58 from 'bs58'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -13,4 +14,12 @@ export function sleep(ms: number) {
 export const toSolanaAmount = (amount: number) => {
   if (!amount) return 0
   return amount / LAMPORTS_PER_SOL
+}
+
+export const toBase58 = (data: Buffer) => {
+  return bs58.encode(data)
+}
+
+export const toHex = (data: Buffer) => {
+  return data.toString('hex')
 }
